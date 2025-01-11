2b632fca6cd098daf527634bbdcc20e7a019a282
tione25 <theonebluesky95@gmail.com>
2025-01-11 22:57:49 +0200

    updates for deploy frontend too

--- front-lms/src/utils/client.ts
+++ front-lms/src/utils/client.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 export const client = axios.create({
-  baseURL: "http://localhost:8002/api",
-  //baseURL: 'http://127.0.0.1:8000/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8002/api",
   headers: {
     Authorization: `Bearer ${localStorage.getItem("authToken") || ""}`, // Include token from localStorage
   },
